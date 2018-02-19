8a8ba76e053efff1c2e13d462b2abfeffe6ab277
Ethan Witherington <wyddindustries@gmail.com>
2018-02-19 17:12:35 -0500

    Added some comments to the main files if blocks.

--- oceanview/oceanview.py
+++ oceanview/oceanview.py
@@ -27,12 +27,17 @@ def main():
     main function for the program
     :return: None
     """
-    # Handle order: Cleardb, Testdb, front/back/both, help
+
+    # Has oceanview done something? If this is still false by the end,
+    # Display the Usage information.
     did_something = False
+
+    # The user wants to clear the database.
     if 'cleardb' in sys.argv:
         did_something = True
-        print("Database Clearing not yet implemented.")
+        print("It's sqlite, just delete the file.")
 
+    # The user wants the test data added to the database.
     if 'maketestdb' in sys.argv:
         did_something = True
         database = data.Database("db.sqlite", "database/build_db.sql")
@@ -42,15 +47,19 @@ def main():
         print("Keystrokes added to test IP 127.127.127.127")
         print("Test files/screenshots not yet implemented.")
 
+    # The user wants the front end launched
     if 'front' in sys.argv or 'both' in sys.argv:
         did_something = True
         frontend = front.init()
         frontend.run('127.0.0.1', 8000)
+
+    # The user wants the back end launched.
     if 'back' in sys.argv or 'both' in sys.argv:
         did_something = True
         backend = back.init()
         backend.run('127.0.0.1', 80)
 
+    # did_something is False, nothing was done, show the usage info.
     if did_something is False:
         print("Usage: python oceanview.py [command]")
         print("COMMANDS:")
