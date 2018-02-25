ea44a45a21d8989c7889228eafd348cf0d8a6400
Chris Vantine <justchris.vantine@gmail.com>
2018-02-25 01:37:54 -0500

    Fixed caching, keystroke encoding

--- oceanview/front.py
+++ oceanview/front.py
@@ -76,6 +76,18 @@ def init():
         """Return code 418, as this is a teapot."""
         return "<h1>418</h1>", 418
 
+    @app.after_request
+    def add_header(r):
+        """
+        Add headers to both force latest IE rendering engine or Chrome Frame,
+        and also to cache the rendered page for 10 minutes.
+        """
+        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Pragma"] = "no-cache"
+        r.headers["Expires"] = "0"
+        r.headers['Cache-Control'] = 'public, max-age=0'
+        return r
+
     return app
 
 
