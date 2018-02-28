d4e92434442672ea22795c12b292a4cb831af0aa
Ethan Witherington <wyddindustries@gmail.com>
2018-02-27 23:57:02 -0500

    index.js future friendly, for constructing tags from json

--- oceanview/static/index.js
+++ oceanview/static/index.js
@@ -1,11 +1,16 @@
-
-function addtag(input){
+var addTagFromInput = function(input){
     // First of all, make sure it's legit.
     if(input.value==""){
         return;
     }
-    //Building a new tag element, from the ground up.
-
+    addTag(input.value, input.previousElementSibling);
+    input.value = "";
+}
+/*
+text: The text of the tag to add.
+target: The element to put the tag inside of.
+*/
+var addTag = function(text, target){
     // We'll need a delete button.
     var tagx = document.createElement('a');
     tagx.classList.add("tagX");
@@ -14,24 +19,21 @@ function addtag(input){
     tagx.addEventListener("click", function(){
         tagx.parentNode.parentNode.removeChild(tagx.parentNode);
     }, false);
-
     // We'll need a name span for the tag
     var tagnamespan = document.createElement('span');
-    tagnamespan.appendChild(document.createTextNode(input.value));
-
+    tagnamespan.appendChild(document.createTextNode(text));
     // Create the parent tag element
     var newtag = document.createElement('div');
     newtag.classList.add("tag");
-    newtag.style.backgroundColor = hashToColor(hashString(input.value));
+    newtag.style.backgroundColor = hashToColor(hashString(text));
     newtag.appendChild(tagnamespan);
     newtag.appendChild(tagx);
-
     // Attach our new tag to the tags container.
-    input.previousElementSibling.appendChild(newtag);
-
-    // wipe the input box to reset it.
-    input.value = "";
+    target.appendChild(newtag);
 }
+
+// ~~~~~~~~~~~~~~~~~~~~  Tag Color Generation. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 var hashString = function(str){
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
