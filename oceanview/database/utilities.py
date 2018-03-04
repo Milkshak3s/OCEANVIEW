516604eeff4d7e58701f5f51269adfbb7b5aa40f
Ethan Witherington <wyddindustries@gmail.com>
2018-03-03 22:14:35 -0500

    Test Data Better, Will Crash

--- oceanview/database/utilities.py
+++ oceanview/database/utilities.py
@@ -6,16 +6,15 @@ Author: Micah Martin
 import time
 
 TEST_TEXT = [
-    b"<LeftShiftDown>N<LeftShiftUp>ever",
-    b"Gonna",
-    b"Give",
-    b"You",
-    b"Up",
-    b"This line is very very long, so that the web dev person can make sure\
-    that stupid long lines don't break the frontend UI.",
-    b"rm -rf /*",
-    b"There is no need to be concerned",
-    b"LOOK AT THE HAPPY DOG, JOE."
+    b"<5p3c1@l> Chars work &<|>",
+    b"This text data was inserted by passing OCEANVIEW the 'maketestdb' command.",
+    b"This is a Test IP."
+]
+
+TEST_IPS = [
+    "127.0.0.2",
+    "127.0.0.3",
+    "127.0.0.4"
 ]
 
 
@@ -37,41 +36,10 @@ def validate_ip(addr): # W0613 suppresses addr not used warning.
 
 def add_test_data(database):
     """ Put test data into the database """
-    print("Adding fake keystrokes...")
-    for stringy in TEST_TEXT:
-        database.add_keystroke(addr='127.127.127.127', keystroke=stringy)
-        time.sleep(1)
-    print("Adding fake images...")
-    database.add_file(addr='127.127.127.127', filename='static/wave.jpg')
-    time.sleep(1)
-    database.add_file(addr='127.127.127.127', filename='static/dog.jpg')
-    print("Adding Tags to test IP...")
-    database.add_tag("127.127.127.127", "testing")
-    time.sleep(1)
-    database.add_tag("127.127.127.127", "guac")
-    time.sleep(1)
-    database.add_tag("127.127.127.127", "guacamole.")
-    print("Adding tags to more IPs...")
-    database.add_tag("127.127.127.0", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.1", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.2", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.3", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.4", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.5", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.6", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.7", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.8", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.9", "guacamole.")
-    time.sleep(1)
-    database.add_tag("127.127.127.10", "guacamole.")
-    time.sleep(1)
+    print("Adding test data...")
+    for text in TEST_TEXT:
+        for ip in TEST_IPS: # pylint: disable=C0103
+            database.add_keystroke(ip, text)
+            database.add_file(ip, 'static/dog.jpg')
+            database.add_tag(ip, "test")
     print("Test Data Added.")
